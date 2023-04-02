0abce7450531f0bf9e5e4b7d3fbc5da780cf9f8a
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-02 01:25:50 -0500

    Slider added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,23 @@
-const button1 = document.querySelector(#button1);
-const button2 = document.querySelector(#button2);
-const button3 = document.querySelector(#button3);
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const spidermanModal = document.querySelector("#spidermanModal");
+const robbinModal = document.querySelector("#robbinModal");
+const batmanModal = document.querySelector("#batmanModal");
+const closeX = document.querySelector(".modal__content--closeIcon");
+const modalSection = document.querySelector(".modal__content");
+const modalPage = document.querySelector(".modal");
+
+button1.addEventListener("click", () => {
+  spidermanModal.classList.remove("hidden");
+});
+
+document.addEventListener("click", function (event) {
+  if (!modalSection.contains(event.target) && event.target !== button1) {
+    spidermanModal.classList.add("hidden");
+  }
+});
+
+closeX.addEventListener("click", function () {
+  modalPage.classList.add("hidden");
+});
